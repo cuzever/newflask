62216586c725ed836161ceec4e271c9062b13248
cuzever <cuzever@gmail.com>
2019-05-20 21:05:22 +0800

    db models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+import datetime
 
 
 #加载用户的回调函数
@@ -47,17 +48,98 @@ class User(UserMixin, db.Model):
         return '<User %r>' % slef.username
 
 
-#工厂表
-class Factory(db.Model):
-    __tablename__ = 'factory'
-    id = db.Column(db.String(20), primary_key=True)
-    address = db.Column(db.String(40))
-    responsor = db.Column(db.String(20))
-
-
 #设备表
 class Equipment(db.Model):
     __tablename__ = 'equipment'
-    id = db.Column(db.String(20),primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
+    fid = db.Column(db.String(20))
+    eid = db.Column(db.String(20))
     place = db.Column(db.String(20), index=True)
-    supplier = db.Column(db.String(20)
+    supplier = db.Column(db.String(20))
+    spl_address = db.Column(db.String(100))
+    spl_contact = db.Column(db.String(20))
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    SencerNum = db.Column(db.Integer)
+    SencerName = db.Column(db.String(125))
+    NoLoad_set = db.Column(db.String(100))
+    EmptyLoad_set = db.Column(db.String(100))
+    Temp = db.Column(db.Float)
+    Wet = db.Column(db.Float)
+    ExcV = db.Column(db.Float)
+    Sensitivity = db.Column(db.Float)
+    Resistance = db.Column(db.Integer)
+    Thread = db.relationship('Thread',backref='Eqp')
+    stateList = db.relationship('stateList',backref='Eqp')
+    NewVal = db.relationship('NewVal',backref='Eqp')
+    FaultMsg = db.relationship('FaultMsg',backref='Eqp')
+    __table_args__ = (
+        db.UniqueConstraint('fid', 'eid', name='uix_fid_eid'),#联合唯一索引
+    )
+
+class Thread(db.Model):
+    __tablename__ = 'thread'
+    id = db.Column(db.Integer, primary_key=True)
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    standard = db.Column(db.Float)
+    zeropoint = db.Column(db.Float)
+    Eid = db.Column(db.Integer,db.ForeignKey('equipment.id'))
+
+class stateList(db.Model):
+    __tablename__ = 'statelist'
+    id = db.Column(db.Integer, primary_key=True)
+    FaultTime = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    RecoverTime = db.Column(db.DateTime)
+    PeriodSecond = db.Column(db.Integer)
+    FaultSencer = db.Column(db.String(20), index=True)
+    FaultCode = db.Column(db.Integer, index=True)
+    FaultState = db.Column(db.Boolean)
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    record = db.Column(db.String(250))
+    Eid = db.Column(db.Integer,db.ForeignKey('equipment.id'))
+
+class NewVal(db.Model):
+    __tablename__ = 'newval'
+    id = db.Column(db.Integer, primary_key=True)
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    WeightTag1 = db.Column(db.Float)
+    WeightTag2 = db.Column(db.Float)
+    WeightTag3 = db.Column(db.Float)
+    WeightTag4 = db.Column(db.Float)
+    Weight = db.Column(db.Float)
+    Eid = db.Column(db.Integer,db.ForeignKey('equipment.id'))
+
+class FaultMsg(db.Model):
+    __tablename__ = 'faultmsg'
+    id = db.Column(db.Integer, primary_key=True)
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
+    Partial = db.Column(db.Integer)
+    Forced = db.Column(db.Integer)
+    Loss = db.Column(db.Integer)
+    Over = db.Column(db.Integer)
+    eqpState = db.Column(db.Integer)
+    Eid = db.Column(db.Integer,db.ForeignKey('equipment.id'))
+
+class test_water(db.Model):
+    __tablename__ = 'scale_PredWaterData'
+    __bind_key__ = 'test'
+    id = db.Column(db.Integer, primary_key=True)
+    Timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+    WeightTag1 = db.Column(db.Float)
+    WeightTag2 = db.Column(db.Float)
+    WeightTag3 = db.Column(db.Float)
+    WeightTag4 = db.Column(db.Float)
+    Weight = db.Column(db.Float)
+    PredW1 = db.Column(db.Float)
+    PredW2 = db.Column(db.Float)
+    PredW3 = db.Column(db.Float)
+    PredW4 = db.Column(db.Float)
+    PredW = db.Column(db.Float)
+    PartialLoadFlag = db.Column(db.Integer)
+    SensorTag1 = db.Column(db.Integer)
+    SensorTag2 = db.Column(db.Integer)
+    SensorTag3 = db.Column(db.Integer)
+    SensorTag4 = db.Column(db.Integer)
+    EQP_State = db.Column(db.Integer)
+    Normal = db.Column(db.Integer)
+    AlertF = db.Column(db.Integer)
+    AlarmF = db.Column(db.Integer)
